1806afe4defb2e545fbdba212e4d6d6801118ea8
Hoang D Vo <hoangvod@gmail.com>
2019-09-25 22:14:00 -0500

    Update hw1.cpp

--- hw1.cpp
+++ hw1.cpp
@@ -30,34 +30,54 @@ int size;
 bool **image;
 
 void renderPixel(int x, int y) {
+    /*input: integer values x and y which are location of the
+       pixel (x,y) on the image
+      output: nothing
+      function: light up the pixel (x,y) and its symmetric
+      counterpart (y,x)
+    */
 	assert(x >= 0 && y >= 0 && x <= size && y <= size);
 	image[x][y] = 1;
 
 	// TODO:  light up the pixel's symmetric counterpart
+
     //  The arch is 1/4 circle, at the first section, so
-    //symmetric counter part is the reflected image between the line of 45 degree, counter part of (x,y) is (y,x)
+    //symmetric counter part is the reflected image between
+    // the line of 45 degree, counter part of (x,y) is (y,x)
     image[y][x] = 1;
 
 }
 
 void rasterizeArc(int radius) {
+    /*Input: the integer value radius, the radius r of a circle
+     output: nothing
+     function: using the radius and renderPixel to draw an arc of 1/4 circle
+     applying the midpoint circle algorithm
+     */
 	// TODO:  rasterize the arc using renderPixel to light up pixels
     int x = 0;
     int y = radius;
-    //d=(x + 1)^2 + (y+1/2)^2 -radius^2
-    //at the beginning, x=0, y=radius =>
+    //d=(x + 1)^2 + (y-1/2)^2 -radius^2
+    //at the beginning x=0, y=radius =>
     double d= 5.0 / 4.0 - radius;
     renderPixel(x, y);
+
+    //Begin drawing pixel
     while (y > x) {
+    //if d_old <0 ->next midpoint will be one increment over in x
       if (d < 0) {
+          //d_new = F(x+1 +1, y-1/2), the point outside circle
          d += 2.0 * x + 3.0;
       }
       else {
+          //if d_old>0-> the next midpoint will be one increment over in x
+          // and one increment down in y
+          //d_new =F(x+1,y-1/2-1), the point inside circle
          d += 2.0 * (x- y) + 5.0;
          y--;
       }
       x++;
-      renderPixel(x, y);
+      renderPixel(x, y); //at the new pixel, we light up
      }
 }
 
